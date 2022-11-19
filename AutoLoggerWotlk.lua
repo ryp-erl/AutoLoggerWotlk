7cd0c53de4cd2372ac16ce6da4aa17d2b46ee48e
Ryp <ryp.erl@pm.me>
2022-11-19 20:17:49 +0100

    chore: first commit

---
+++ AutoLoggerWotlk.lua
@@ -0,0 +1,63 @@
+AutoLoggerWotlk = AutoLoggerWotlk or LibStub("AceAddon-3.0"):NewAddon("AutoLoggerWotlk", "AceConsole-3.0", "AceEvent-3.0")
+
+AutoLoggerWotlk.name = "AutoLoggerWotlk"
+AutoLoggerWotlk.version = "1.0.0"
+
+AutoLoggerWotlk.raids = {
+    -- Naxxramas
+    [533] = true,
+    -- The Obsidian Sanctum
+    [615] = true,
+    -- The Eye Of Eternity
+    [616] = true,
+    -- Vault of Archavon
+    [624] = true,
+    -- Ulduar
+    [603] = true,
+    -- Trial of the Crusader
+    [649] = true,
+    -- Onyxia's Lair
+    [249] = true,
+    -- Icecrown Citadel
+    [631] = true,
+    -- The Ruby Sanctum
+    [724] = true,
+}
+--[[
+    Initializing the addon using Ace3.
+    It includes options, commands, saved variables and basic events to register to.
+--]]
+
+function AutoLoggerWotlk:OnInitialize()
+end
+
+function AutoLoggerWotlk:OnEnable()
+    self:RegisterEvent("UPDATE_INSTANCE_INFO")
+    AutoLoggerWotlk:CheckInstanceAndLog()
+end
+
+function AutoLoggerWotlk:OnDisable()
+end
+
+--[[
+    Events
+--]]
+
+function AutoLoggerWotlk:UPDATE_INSTANCE_INFO()
+    AutoLoggerWotlk:CheckInstanceAndLog()
+end
+
+--[[
+    Core functions
+--]]
+
+function AutoLoggerWotlk:CheckInstanceAndLog()
+    local name, _, _, _, _, _, _, instanceID, instanceGroupSize, _ = GetInstanceInfo()
+    if AutoLoggerWotlk.raids[tonumber(instanceID)] then
+        LoggingCombat(true)
+        self:Print("Combat logging |cff33ff00activated|r for " .. name .. " (" .. instanceGroupSize .. ").")
+    else
+        self:Print("Combat logging |cffff0000deactivated|r (not in a WotLK raid).")
+        LoggingCombat(false)
+    end
+end
---
+++ README.md
@@ -0,0 +1,22 @@
+<img src="https://github.com/ryp-erl/assets/blob/main/AutoLoggerWotlk/autologgerwotlk_logo.jpg?raw=true" width=50 align="right" />
+
+![version](https://img.shields.io/github/v/release/En-Roue-Libre/AutoLoggerWotlk)
+[![issues](https://img.shields.io/badge/report-issues-blueviolet)](https://github.com/En-Roue-Libre/AutoLoggerWotlk/issues)
+
+# AutoLoggerWotlk
+
+Automatically start combat logging when entering a Wotlk raid:
+
+- Naxxramas
+- The Obsidian Sanctum
+- The Eye Of Eternity
+- Vault of Archavon
+- Ulduar
+- Trial of the Crusader
+- Onyxia's Lair
+- Icecrown Citadel
+- The Ruby Sanctum
+
+## License
+
+[GNU GPLv3](LICENSE)
